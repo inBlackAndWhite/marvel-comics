30afdd61ef0724706b35a31f99b1e12a74ab7921
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-03-19 21:50:34 -0300

    [comics-list] Default image and description for comics with missing info

--- src/app/comic/comic.component.ts
+++ src/app/comic/comic.component.ts
@@ -11,24 +11,41 @@ export class ComicComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.addImageSrc();
+    this.addDescription();
   }
 
   @Input() comic: Object;
+  description: string;
+  imageSrc: string;
 
-  comicImage(): string {
+  buildImageSrc(): string {
     let image = this.comic['images'][0];
 
-    if (image) {
-      let path = image['path'];
-      let extension = image['extension'];
-      let size = 'portrait_incredible';
+    let path = image['path'];
+    let extension = image['extension'];
+    let size = 'portrait_incredible';
 
-      return `${path}/${size}.${extension}`;
-    }
+    return `${path}/${size}.${extension}`;
   }
 
   formatDate(date: string): string {
     date = date.replace(/^-/, '');
     return moment(date.toString()).format('MMMM DD, YYYY');
+  }
+
+  addDescription(): void {
+    let loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae alias quaerat dolores facilis autem totam sit, harum, possimus enim saepe commodi ea animi eligendi magni. Deserunt, deleniti, labore nihil provident facilis aspernatur accusantium voluptatem? Labore ad tempore, molestias quia officia sapiente tenetur provident velit asperiores.';
+
+    this.description = this.comic['description'] || loremIpsum;
+  }
+
+  addImageSrc(): void {
+    if (this.comic['images'].length === 0) {
+      this.imageSrc = '../../assets/images/iron-man.png';
+      return;
+    }
+
+    this.imageSrc = this.buildImageSrc();
   }
 }
