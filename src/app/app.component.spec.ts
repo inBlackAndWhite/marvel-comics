3f53289b80b00a217581a85f7c5f8fbadf2f758e
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-03-25 10:53:17 -0300

    [jasmine] App Component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,43 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, inject } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
+import { ComicsListComponent } from './comics-list/comics-list.component';
+import { ComicComponent } from './comic/comic.component';
+import { ComicDetailComponent } from './comic-detail/comic-detail.component';
+import { LoaderService } from './services/loader.service';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        ComicsListComponent,
+        ComicComponent,
+        ComicDetailComponent
       ],
+      providers: [LoaderService]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+
+  it('should create the app', async(inject([LoaderService], (loaderService: LoaderService) => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
+  })));
+
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+
+    expect(compiled.querySelector('h1').textContent).toContain('Iron Man');
+  }));
+
+  it('should render app-comics-list component', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('app-comics-list')).toBeTruthy();
   }));
 });
